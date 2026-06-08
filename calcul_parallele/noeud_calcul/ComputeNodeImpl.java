3ed6c7ad1cf4ed8d4ae6e3ab73470a5501e74018
Fousse Emelyne <emelyne.fousse8@etu.univ-lorraine.fr>
2026-06-08 11:32:22 +0200

    refonte noeuds : changement du fonctionnement : les noeuds n'appellent plus le dispatcher pour travailler mais sont enregistrés dans le service central

--- calcul_parallele/noeud_calcul/ComputeNodeImpl.java
+++ calcul_parallele/noeud_calcul/ComputeNodeImpl.java
@@ -1,53 +1,59 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.io.File;
-import java.io.PrintWriter;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
 import java.time.Instant;
 import java.time.Duration;
-
 import raytracer.Image;
 import raytracer.Scene;
 
-public class ComputeNodeImpl {
+public class ComputeNodeImpl implements ComputeNode {
 
-    public static void main(String[] args) {
-        demarrerNoeud("Noeud-1");
+    private String nom;
+
+    public ComputeNodeImpl(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public Image calculerBloc(String fichier_description, int x, int y, int w, int h, int largeurTotale, int hauteurTotale) throws RemoteException {
+        System.out.println(nom + " : Tâche reçue : bloc de " + w + "x" + h + " en (" + x + "," + y + ")");
+        Instant debut = Instant.now();
+
+        // Calcul local du bloc
+        Scene scene = new Scene(fichier_description, largeurTotale, hauteurTotale);
+        Image bloc = scene.compute(x, y, w, h);
+
+        Instant fin = Instant.now();
+        long duree = Duration.between(debut, fin).toMillis();
+        System.out.println(nom + " : Tâche complétée en " + duree + " ms.");
+        
+        // Retourne le résultat
+        return bloc;
     }
 
     public static void demarrerNoeud(String nomNoeud) {
         try {
-            // Recherche du Dispatcher
+            ComputeNodeImpl node = new ComputeNodeImpl(nomNoeud);
+            // Exportation noeud pour qu'il devienne un serveur RMI accessible
+            ComputeNode stub = (ComputeNode) UnicastRemoteObject.exportObject(node, 0);
+
+            // Connexion à l'annuaire RMI
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
-            DispatcherInterface dispatcher = (DispatcherInterface) registry.lookup("Dispatcher");
-            System.out.println(nomNoeud + " : Connecté");
+            ServiceCentral serviceCentral = (ServiceCentral) registry.lookup("ServiceCentral");
 
-            // Boucle infinie pour demander du travail
-            while (true) {
-                Task t = dispatcher.demanderTravail();
+            // Enregistrement du noeud dans la liste du service central
+            serviceCentral.enregistrerNoeud(stub);
+            System.out.println(nomNoeud + " : Enregistréauprès du service central.");
 
-                if (t == null) {
-                    Thread.sleep(1000);
-                } else {
-                    System.out.println(nomNoeud + " : Tâche " + t.id + " reçue : bloc de " + t.w + "x" + t.h + " en ("
-                            + t.x + "," + t.y + ")");
-
-                    Instant debut = Instant.now();
-
-                    Scene scene = new Scene(t.nomFichier, t.largeurTotale, t.hauteurTotale);
-                    Image bloc = scene.compute(t.x, t.y, t.w, t.h);
-
-                    // 3. Renvoyer le résultat
-                    TaskResult resultat = new TaskResult(t.id, bloc);
-                    dispatcher.renvoyerResultat(resultat);
-
-                    Instant fin = Instant.now();
-                    long duree = Duration.between(debut, fin).toMillis();
-                    System.out.println(nomNoeud + " : Tâche " + t.id + " : " + duree + " ms");
-                }
-            }
         } catch (Exception e) {
-            System.err.println(nomNoeud + " : Erreur dans le noeud de calcul : " + e.toString());
+            System.err.println(nomNoeud + " : Erreur d'initialisation : " + e.toString());
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        String nom = args.length > 0 ? args[0] : "Noeud-1";
+        demarrerNoeud(nom);
     }
 }
